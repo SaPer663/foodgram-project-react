5c2352ac7ed4ea4371d6df777cd394d2d7ba3b11
SaPer663 <saper663ru@gmail.com>
2022-06-04 14:37:11 +0500

    feat: добавляет валидацию и меняет длину некоторых полей

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,17 +1,28 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
+from pytils.translit import slugify
 
 User = get_user_model()
 
 TWENTY_LENGTH = 20
 ONE_HUNDRED_LENGTH = 100
+TWO_HUNDRED_LENGTH = 200
 
 
 class Tag(models.Model):
     """Тэг рецептов."""
-    name = models.CharField('название', max_length=ONE_HUNDRED_LENGTH)
-    color = models.CharField('Цветовой HEX-код', max_length=TWENTY_LENGTH)
-    slug = models.SlugField('унифицированное имя тэга', unique=True)
+    name = models.CharField(
+        'название',
+        max_length=ONE_HUNDRED_LENGTH,
+        unique=True
+    )
+    color = models.CharField('Цветовой HEX-код', max_length=7)
+    slug = models.SlugField(
+        'унифицированное имя тэга',
+        max_length=TWO_HUNDRED_LENGTH,
+        unique=True
+    )
 
     class Meta:
         ordering = ('name',)
@@ -21,17 +32,22 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
 class Ingredient(models.Model):
     """Ингридиент рецепта."""
     name = models.CharField(
         'название',
-        max_length=ONE_HUNDRED_LENGTH,
+        max_length=TWO_HUNDRED_LENGTH,
         db_index=True
     )
     measurement_unit = models.CharField(
         'единица измерения',
-        max_length=ONE_HUNDRED_LENGTH
+        max_length=TWO_HUNDRED_LENGTH
     )
 
     class Meta:
@@ -48,28 +64,28 @@ class Recipe(models.Model):
     tags = models.ManyToManyField(
         Tag,
         through='RecipeTags',
+        related_name='recipes',
         verbose_name='тэги'
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='автор',
-        related_name='recipe'
+        related_name='recipes'
     )
     ingredients = models.ManyToManyField(
         Ingredient,
-        through='RecipeIngredientQuantity',
+        through='IngredientAmount',
+        related_name='recipes',
         verbose_name='ингредиенты'
     )
-    is_favorited = models.BooleanField('в избранном', default=False)
-    is_in_shopping_cart = models.BooleanField(
-        'в списке покупок',
-        default=False
-    )
-    name = models.CharField('название', max_length=ONE_HUNDRED_LENGTH)
+    name = models.CharField('название', max_length=TWO_HUNDRED_LENGTH)
     image = models.ImageField('изображение', upload_to='recipes/')
     text = models.TextField('описание')
-    cooking_time = models.PositiveSmallIntegerField()
+    cooking_time = models.PositiveSmallIntegerField(
+        'время приготовления',
+        validators=(MinValueValidator(1),)
+    )
 
     class Meta:
         ordering = ('name',)
@@ -80,26 +96,37 @@ class Recipe(models.Model):
         return self.name
 
 
-class RecipeIngredientQuantity(models.Model):
+class IngredientAmount(models.Model):
     """Количество ингредиента в рецепте."""
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
+        related_name='ingredient_amounts',
         verbose_name='ингредиент'
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
+        related_name='ingredient_amounts',
         verbose_name='рецепт'
     )
-    amount = models.PositiveSmallIntegerField()
+    amount = models.PositiveSmallIntegerField(
+        'количество',
+        validators=(MinValueValidator(1),),
+    )
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['ingredient', 'recipe'],
+                name='unique_ingredient'
+            ),
+        ]
         verbose_name = 'Количество ингредиента'
         verbose_name_plural = 'Количество ингредиентов'
 
     def __str__(self):
-        return f'{self.ingredient.title}-{self.amount}'
+        return f'{self.ingredient.name}-{self.amount}'
 
 
 class RecipeTags(models.Model):
