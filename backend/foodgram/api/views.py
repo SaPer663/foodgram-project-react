d0e40bd53ee9d046742eea887bd00622926ca943
SaPer663 <saper663ru@gmail.com>
2022-06-04 14:43:01 +0500

    feat: добавляет предсталение для рецептов

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from api.serializers import IngredientsSerializer, TagsSerializer
-from recipes.models import Ingredient, Tag
+from api.serializers import (
+    IngredientsSerializer, RecipesForReadingSerializer,
+    RecipesForWritingSerializer, TagsSerializer,
+)
+from recipes.models import Ingredient, Recipe, Tag
+
+User = get_user_model()
+user = User.objects.get(username='saper663')
 
 
 class TagsViewSet(ReadOnlyModelViewSet):
@@ -9,6 +16,19 @@ class TagsViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
 
 
-class IngredientViewSet(ReadOnlyModelViewSet):
+class IngredientsViewSet(ReadOnlyModelViewSet):
     serializer_class = IngredientsSerializer
     queryset = Ingredient.objects.all()
+
+
+class RecipesViewSet(ModelViewSet):
+
+    queryset = Recipe.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=user)
+
+    def get_serializer_class(self):
+        if self.action in ('retrieve', 'list'):
+            return RecipesForReadingSerializer
+        return RecipesForWritingSerializer
