10cdafdda7ef379d1d79951ac79a577bb0b93da5
SaPer663 <saper663ru@gmail.com>
2022-06-04 14:32:42 +0500

    feat: добавляет описание моделей в админке

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import (
+    Ingredient, IngredientAmount, Recipe, RecipeTags, Tag,
+)
+
+
+class RecipeTagsInline(admin.TabularInline):
+    """Тэги рецепта."""
+    model = RecipeTags
+    extra = 1
+
+
+class IngredientAmountInline(admin.TabularInline):
+    """Количество ингредиента."""
+    model = IngredientAmount
+    extra = 1
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    """Тэги."""
+    list_display = ('name', 'color', 'slug',)
+    search_fields = ('name',)
+    list_filter = ('color',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Ingredient)
+class IngredientsAdmin(admin.ModelAdmin):
+    """Ингредиенты."""
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name', 'measurement_unit',)
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    """Рецепты."""
+    list_display = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name',)
+    filter_horizontal = ('ingredients',)
+    inlines = (RecipeTagsInline, IngredientAmountInline)
