4909a04e4a36b50efb43fb568f46b24eb14ab4f3
SaPer663 <saper663ru@gmail.com>
2022-06-16 22:24:26 +0500

    feat: добавляет функционал добаления ингредиентов рецепта в список покупок и скачивание его

--- backend/foodgram/api/recipe_views.py
+++ backend/foodgram/api/recipe_views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F, Sum
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from api.filters import AuthorAndTagFilter
@@ -12,8 +15,9 @@ from api.recipe_serializers import (
     FavoritesSerializer, IngredientsSerializer, RecipesForReadingSerializer,
     RecipesForWritingSerializer, TagsSerializer,
 )
-from core.pagination import LimitPagePagination
-from recipes.models import Favorites, Ingredient, Recipe, Tag
+from api.user_serializers import RecipeMinified
+from api.utilits import LimitPagePagination, get_shopping_cart_pdf
+from recipes.models import Favorites, Ingredient, Recipe, Shopping_cart, Tag
 from users.permissions import AuthorOrReadOnly
 
 User = get_user_model()
@@ -56,6 +60,9 @@ class RecipesViewSet(ModelViewSet):
         permission_classes=(permissions.IsAuthenticated,),
     )
     def favorite(self, request, pk):
+        """
+        Добавление(удаление) рецептов в(из) список(ка) избранных рецептов.
+        """
         recipe = get_object_or_404(Recipe, pk=pk)
         if request.method == 'DELETE':
             try:
@@ -78,3 +85,53 @@ class RecipesViewSet(ModelViewSet):
         data.pop('recipe')
         data.pop('user')
         return Response(data=data, status=status.HTTP_201_CREATED)
+
+    @action(
+        methods=['post', 'delete'],
+        detail=True,
+        permission_classes=(permissions.IsAuthenticated,),
+    )
+    def shopping_cart(self, request, pk):
+        """
+        Добавление(удаление) ингредиентов рецепта в(из) список(ка) покупок
+        """
+        recipe = get_object_or_404(Recipe, pk=pk)
+        in_shopping_cart = Shopping_cart.objects.filter(
+            recipe=recipe, user=request.user
+        ).exists()
+        if request.method == 'DELETE':
+            if not in_shopping_cart:
+                raise ValidationError(
+                    {'recipe': 'Рецепта нет в списке покупок.'}
+                )
+            Shopping_cart.objects.get(
+                recipe=recipe, user=request.user
+            ).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        if in_shopping_cart:
+            raise ValidationError(
+                {'recipe': 'Рецепт уже в списке покупок.'}
+            )
+        Shopping_cart.objects.create(
+            recipe=recipe, user=request.user
+        )
+        return Response(
+            RecipeMinified(instance=recipe).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(
+        detail=False,
+        permission_classes=(permissions.IsAuthenticated,)
+    )
+    def download_shopping_cart(self, request):
+        """Скачивание списка покупок."""
+        shopping_cart_queryset = request.user.shopping_carts.values(
+            name=F('recipe__ingredient_amounts__ingredient__name'),
+            unit=F('recipe__ingredient_amounts__ingredient__measurement_unit')
+        ).annotate(amount=Sum('recipe__ingredient_amounts__amount'))
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = (
+            'attachment; ' 'filename="shopping_cart.pdf"'
+        )
+        return get_shopping_cart_pdf(request, shopping_cart_queryset, response)
