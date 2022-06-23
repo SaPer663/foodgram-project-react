3283a0472ddf6be13125c3af8a058adacf5a430a
SaPer663 <saper663ru@gmail.com>
2022-06-23 20:52:45 +0500

    fix: исправляет работу фильтрации рецептов по тегам

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 
 User = get_user_model()
 
@@ -14,7 +14,12 @@ class AuthorAndTagFilter(FilterSet):
     `is_in_shopping_cart` фильтрует по рецептам, ингредиенты которых добавленны
     в список покупок.
     """
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        lookup_expr='icontains',
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
