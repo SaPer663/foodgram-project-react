a48a74c7839654fc9786078f27abdfe73678a6e7
SaPer663 <saper663ru@gmail.com>
2022-06-25 08:39:15 +0500

    fix: изменяет разрешения для получения информации о пользователях

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -142,8 +142,8 @@ DJOSER = {
 
     'LOGIN_FIELD': 'email',
     'PERMISSIONS': {
-        'user_list': ('rest_framework.permissions.AllowAny',),
-        'user': ('djoser.permissions.CurrentUserOrAdminOrReadOnly',)
+        'user_list': ('djoser.permissions.CurrentUserOrAdmin',),
+        'user': ('djoser.permissions.CurrentUserOrAdmin',)
     },
     'SERIALIZERS': {
         'user': 'api.user_serializers.CustomUserSerializer',
