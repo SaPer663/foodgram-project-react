f0d589ec94b2e3bffc4bf2c4b8c032c4f409494f
SaPer663 <saper663ru@gmail.com>
2022-07-03 18:24:46 +0500

    fix: удаляет неннужный файл

--- apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.contrib import auth
-
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
-def fim():
-    pass
